5465d2bfe239ea80041dd1566e4e9dc85905895e
GlobalCodes <glorychukwuzurum@gmail.com>
2026-06-01 16:16:00 +0000

    feat: implement new updates

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
     return (
-        <SafeAreaView className="flex-1 items-center justify-center bg-background">
+        <SafeAreaView className="flex-1 bg-background p-5">
             <Text className="text-xl font-bold text-success">
                 Welcome to Nativewind!
             </Text>
