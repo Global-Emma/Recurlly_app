4aef7a7fef085e1c944c5e63d663df776bed666a
GlobalCodes <glorychukwuzurum@gmail.com>
2026-06-08 11:53:11 +0000

    feat: implement the homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,104 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import "@/global.css"
+import {FlatList, Text, View} from "react-native";
+import {Link} from "expo-router";
+import {styled} from "nativewind";
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {Image} from "expo-image";
+import images from "@/constants/images";
+import {HOME_SUBSCRIPTIONS, HOME_USER, UPCOMING_SUBSCRIPTIONS} from "@/constants/data"
+import {HOME_BALANCE} from "@/constants/data"
+import {icons} from "@/constants/icons"
+import dayjs from "dayjs";
+import {formatCurrency} from "@/lib/utils";
+import ListHeading from "@/components/listHeading";
+import UpcomingSubscriptionCard from "@/components/upcomingSubscriptionCard";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import {useState} from "react";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+const SafeAreaView = styled(RNSafeAreaView);
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
+export default function App() {
+    const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<string | null>(null)
+    return (
+        <SafeAreaView className="flex-1 bg-background p-5">
+
+
+            <FlatList
+                ListHeaderComponent={
+                    () => {
+                        return (
+                            <>
+                                <View className={"mb-2.5 flex-row items-center justify-between"}>
+                                    <View className={"flex-row items-center"}>
+                                        <Image source={images.avatar} className={"size-16 rounded-full"}/>
+                                        <Text
+                                            className={"ml-4 text-2xl font-sans-bold text-primary"}>{HOME_USER.name}</Text>
+                                    </View>
+
+                                    <View
+                                        className={"flex-row items-center bg-transparent border border-black/20 justify-center rounded-full p-2"}>
+                                        <Image source={icons.add} className={"size-12"}/>
+                                    </View>
+                                </View>
+
+                                <View
+                                    className={"my-2.5 min-h-50 justify-between gap-5 rounded-bl-4xl rounded-tr-4xl bg-accent p-6"}>
+                                    <Text className={"text-xl font-sans-semibold text-white/80"}>Balance</Text>
+
+                                    <View className={"flex-row items-center justify-between"}>
+                                        <Text
+                                            className={"text-4xl font-sans-extrabold text-white"}>{formatCurrency(HOME_BALANCE.amount)}</Text>
+                                        <Text
+                                            className={"text-xl font-sans-medium text-white"}>{dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}</Text>
+                                    </View>
+                                </View>
+
+                                <View className={"mb-5"}>
+                                    <ListHeading title={'Upcoming'}/>
+                                    <FlatList
+                                        data={UPCOMING_SUBSCRIPTIONS}
+                                        renderItem={({item}) => {
+                                            return (
+                                                <UpcomingSubscriptionCard {...item} />
+                                            )
+                                        }}
+                                        keyExtractor={(item) => item.id}
+                                        horizontal={true}
+                                        showsHorizontalScrollIndicator={false}
+                                        ListEmptyComponent={<Text
+                                            className={"py-4 text-sm font-sans-medium text-black/60"}>No upcoming
+                                            renewals yet</Text>}
+                                    />
+                                </View>
+
+                                <ListHeading title={'All Subscriptions'}/>
+
+                            </>
+                        )
+                    }
+                }
+                data={HOME_SUBSCRIPTIONS}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => (
+                    <SubscriptionCard {...item}
+                                      expanded={expandedSubscriptionId === item.id}
+                                      onPress={() => {
+                                          setExpandedSubscriptionId((currentId) => (currentId === item.id ? null : item.id))
+                                      }}
+                    />
+                )}
+                extraData={expandedSubscriptionId}
+                ItemSeparatorComponent={() => (
+                    <View className={"h-1 w-full bg-border"}/>
+                )}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={(
+                    <Text className={"py-4 text-sm font-sans-medium text-black/60"}>No subscriptions
+                        yet</Text>
+                )}
+                contentContainerClassName="pb-30"
             />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+        </SafeAreaView>
+    );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
